60de0aa60ce8051ee3ac264ddd210d4a1cbba910
Ilya <ikalinin667@gmail.com>
2023-03-09 21:22:32 +0600

    123

--- telegram/main.py
+++ telegram/main.py
@@ -6,6 +6,7 @@ def generate_bot(data, sendData):
 
     @bot.message_handler(content_types="text")
     def q(message):
+        sendData({"text":message.text})
         bot.send_message(message.chat.id, data)
     
     print("bot is ready to work!")
