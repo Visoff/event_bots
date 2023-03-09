ae7ed390b0351c7f262b33e74eddcb41c7affaa2
Ilya <ikalinin667@gmail.com>
2023-03-09 21:50:13 +0600

    123

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import websockets
 import time
 import json
 
+bots = {}
+
 data_to_send = {}
 
 def sendData(inp):
@@ -11,13 +13,15 @@ def sendData(inp):
     data_to_send = {**data_to_send, **inp}
 
 async def socket_function():
-    global data, data_to_send
+    global data, data_to_send, bots
     async with websockets.connect('wss://api.visoff.ru') as websocket:
         while True:
             await websocket.send(json.dumps(data_to_send))
             data_to_send = {}
             recv = await websocket.recv()
             data = json.loads(recv)
+            for message in data["telegram"]["needToSend"]:
+                bots.telegram_bot.send_message(123, message)
             print(data)
             await asyncio.sleep(5)
 def main_socket_function():
@@ -29,17 +33,18 @@ socket_thread.start()
 time.sleep(10)
 
 def judge_helper_function():
+    global bots
     from judge_helper_bot.main import generate_judge_helper
-    judge_helper_bot = generate_judge_helper()
-    judge_helper_bot.infinity_polling()
+    bots.judge_helper_bot = generate_judge_helper()
+    bots.judge_helper_bot.infinity_polling()
 judge_helper_thread = threading.Thread(target=judge_helper_function)
 judge_helper_thread.start()
 
 
 def telegram_function():
-    global data, sendData
+    global data, sendData, bots
     from telegram.main import generate_bot
-    telegram_bot = generate_bot(data, sendData)
-    telegram_bot.infinity_polling()
+    bots.telegram_bot = generate_bot(data, sendData)
+    bots.telegram_bot.infinity_polling()
 telegram_thread = threading.Thread(target=telegram_function)
 telegram_thread.start()
