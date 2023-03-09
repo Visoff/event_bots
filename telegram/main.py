b67c3680569a567459cdaf5c137cd970579ed711
Ilya <ikalinin667@gmail.com>
2023-03-09 21:56:30 +0600

    123

--- telegram/main.py
+++ telegram/main.py
@@ -4,11 +4,13 @@ def generate_bot(data, sendData):
     token = "5255432703:AAHr8pBOON_KUih3uC1lTHczGTdkUypYvMo"
     bot = telebot.TeleBot(token=token)
 
-    @bot.message_handler(content_types="text")
+    @bot.message_handler(commands=["send"])
     def q(message):
-        sendData({"text":message.text})
-        print(message.chat.id)
-        bot.send_message(message.chat.id, data)
+        if (message.reply_to_message):
+            print(message.reply_to_message.text)
+        else:
+            sendData({"text":message.text})
+            bot.send_message(message.chat.id, "sent")
     
     print("bot is ready to work!")
     return bot
